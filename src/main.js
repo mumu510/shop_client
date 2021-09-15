45ff9a0981a9e903d0434107e0fe9179fa08b2b0
limenghui <1141955869@qq.com>
2021-09-15 15:43:19 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//浏览器控制台不显示非生产环境的打包
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
